6933240d73eadb0fdb5d89481d9e06c475e772db
Anuvab Biswas <anuvabbiswas12@gmail.com>
2019-02-16 23:34:34 +0530

    Initial commit

---
+++ app/src/main/java/com/anuvab/projects/phpmysql/Constants.java
@@ -0,0 +1,6 @@
+package com.anuvab.projects.phpmysql;
+
+public class Constants {
+    private static final String ROOT_URL="http://10.170.40.149/Android/v1/registerUser.php";
+    public static final String URL_REGISTER=ROOT_URL+"registerUser.php";
+}
